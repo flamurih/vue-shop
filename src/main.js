027d6e9bfdf1ed9bb6d8dbdb3c39915f560f7f77
Flamurih <flamurhoxha1998@gmail.com>
2023-03-04 21:10:14 +0100

    Initialize vue-shop

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import jquery from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import './assets/app.scss';
+
+createApp(App).mount('#app')
